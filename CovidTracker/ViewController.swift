4eaf8a4a340143db4abaa9b94cbc2af6a4641cef
Harun Gunes <h.gunes13@gmail.com>
2020-08-02 16:20:33 +0200

    Initial Commit

---
+++ CovidTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CovidTracker
+//
+//  Created by Harun Gunes on 02/08/2020.
+//  Copyright © 2020 Harun Gunes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
